62164a4681423bb0c19bcc60348f2bed499517c4
Aiswarya AV <153484561+aiswaryaav03@users.noreply.github.com>
2026-04-15 08:10:32 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
-const http = require("http");
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
+const app = express();
 
-    const name = "Aiswarya";
-    const currentTime = new Date();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    res.writeHead(200, {"Content-Type": "text/html"});
-
-    if(req.url === "/"){
-        res.write(`<h2>Hello from ${name}'s Node.js Server</h2>`);
-        res.write(`<p>Current Date and Time: ${currentTime}</p>`);
-    }
-    else if(req.url === "/about"){
-        res.write("<h2>About Page</h2>");
-        res.write("<p>This server is created using Node.js</p>");
-    }
-    else if(req.url === "/contact"){
-        res.write("<h2>Contact Page</h2>");
-        res.write("<p>Email: aiswarya@example.com</p>");
-    }
-    else{
-        res.writeHead(404, {"Content-Type":"text/html"});
-        res.write("<h2>404 - Page Not Found</h2>");
-    }
-
-    res.end();
+app.get("/", (req, res) => {
+res.send("Server working");
 });
 
-server.listen(3000, "0.0.0.0", ()=>{
-    console.log("Server running at http://localhost:3000");
+app.listen(3000,()=>{
+console.log("Server running at http://localhost:3000");
 });
