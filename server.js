852db12ed1e465d150b6a99e9d20693314f74735
Aiswarya AV <153484561+aiswaryaav03@users.noreply.github.com>
2026-04-15 08:25:49 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,65 @@
 const express = require("express");
-const mysql = require("mysql2");
 const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
 
-app.get("/", (req, res) => {
-res.send("Server working");
+let db;
+
+// Connect MongoDB
+async function start() {
+    await client.connect();
+    db = client.db("eventdb");
+    console.log("MongoDB Connected");
+
+    app.listen(3000, () => {
+        console.log("Server running at http://localhost:3000");
+    });
+}
+start();
+
+// 👉 Register API
+app.post("/register", async (req, res) => {
+    const { regno, name, events } = req.body;
+
+    if (!regno || !name || !events) {
+        return res.send("All fields required");
+    }
+
+    if (events.length > 3) {
+        return res.send("Maximum 3 events allowed");
+    }
+
+    const exist = await db.collection("registrations").findOne({ regno });
+
+    if (exist) {
+        return res.send("Duplicate Register Number not allowed");
+    }
+
+    await db.collection("registrations").insertOne({
+        regno,
+        name,
+        events
+    });
+
+    res.send("Registration Successful");
 });
 
-app.listen(3000,()=>{
-console.log("Server running at http://localhost:3000");
+// 👉 Search API
+app.get("/search/:regno", async (req, res) => {
+    const user = await db.collection("registrations")
+        .findOne({ regno: req.params.regno });
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.json({ message: "No record found" });
+    }
 });
